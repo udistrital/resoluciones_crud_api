74fb17e2871863843a150f05732a47604bedb57b
Camilo Torres <camilo@ulises.udistritaloas.edu.co>
2017-10-11 17:10:13 -0500

    Se corrigen las consultas del modelo precontratado.go

--- models/precontratado.go
+++ models/precontratado.go
@@ -2,23 +2,24 @@ package models
 
 import (
 	"fmt"
+
 	"github.com/astaxie/beego/orm"
 )
 
 type Precontratado struct {
-    Id int `orm:"column(id);pk;auto"`
-    PrimerNombre string `orm:"column(primer_nombre)"`
-    SegundoNombre string `orm:"column(segundo_nombre)"`
-    PrimerApellido string `orm:"column(primer_apellido)"`
-    SegundoApellido string `orm:"column(segundo_apellido)"`
-    Documento int `orm:"column(documento)"`
-    Expedicion string `orm:"column(expedicion)"`
-    Categoria string `orm:"column(categoria)"`
-    Dedicacion string `orm:"column(dedicacion)"`
-    HorasSemanales int `orm:"column(horas_semanales)"`
-    Semanas int `orm:"column(semanas)"`
-    ProyectoCurricular int `orm:"column(proyecto_curricular)"`
-    ValorContrato float64 `orm:"column(valor_contrato)"`
+	Id                 int     `orm:"column(id);pk;auto"`
+	PrimerNombre       string  `orm:"column(primer_nombre)"`
+	SegundoNombre      string  `orm:"column(segundo_nombre)"`
+	PrimerApellido     string  `orm:"column(primer_apellido)"`
+	SegundoApellido    string  `orm:"column(segundo_apellido)"`
+	Documento          int     `orm:"column(documento)"`
+	Expedicion         string  `orm:"column(expedicion)"`
+	Categoria          string  `orm:"column(categoria)"`
+	Dedicacion         string  `orm:"column(dedicacion)"`
+	HorasSemanales     int     `orm:"column(horas_semanales)"`
+	Semanas            int     `orm:"column(semanas)"`
+	ProyectoCurricular int     `orm:"column(proyecto_curricular)"`
+	ValorContrato      float64 `orm:"column(valor_contrato)"`
 }
 
 func init() {
@@ -28,7 +29,7 @@ func init() {
 func GetAllPrecontratado(idResolucion string) (arregloIDs []Precontratado) {
 	o := orm.NewOrm()
 	var temp []Precontratado
-	_, err := o.Raw("SELECT administrativa.vinculacion_docente.id id, agora.informacion_persona_natural.primer_nombre primer_nombre, agora.informacion_persona_natural.segundo_nombre segundo_nombre, agora.informacion_persona_natural.primer_apellido primer_apellido, agora.informacion_persona_natural.segundo_apellido segundo_apellido, agora.informacion_persona_natural.num_documento_persona documento, core.ciudad.nombre expedicion, administrativa.escalafon.nombre_escalafon categoria, administrativa.dedicacion.nombre_dedicacion dedicacion, administrativa.vinculacion_docente.numero_horas_semanales horas_semanales, administrativa.vinculacion_docente.numero_semanas semanas, administrativa.vinculacion_docente.id_proyecto_curricular proyecto_curricular FROM agora.informacion_persona_natural, core.ciudad, administrativa.escalafon_persona, administrativa.escalafon, administrativa.dedicacion, administrativa.vinculacion_docente WHERE agora.informacion_persona_natural.id_ciudad_expedicion_documento=core.ciudad.id_ciudad AND agora.informacion_persona_natural.num_documento_persona=administrativa.vinculacion_docente.id_persona AND administrativa.escalafon_persona.id_persona_natural=agora.informacion_persona_natural.num_documento_persona AND administrativa.vinculacion_docente.id_dedicacion=administrativa.dedicacion.id_dedicacion AND administrativa.escalafon_persona.id_escalafon=administrativa.escalafon.id_escalafon AND administrativa.vinculacion_docente.estado=TRUE AND administrativa.vinculacion_docente.id_resolucion = "+idResolucion+";").QueryRows(&temp)
+	_, err := o.Raw("SELECT vd.id id, ipn.primer_nombre, ipn.segundo_nombre, ipn.primer_apellido, ipn.segundo_apellido, ipn.num_documento_persona documento, c.nombre expedicion, e.nombre_escalafon categoria, d.nombre_dedicacion dedicacion, vd.numero_horas_semanales horas_semanales, vd.numero_semanas semanas, vd.id_proyecto_curricular FROM administrativa.vinculacion_docente vd INNER JOIN agora.informacion_persona_natural ipn ON ipn.num_documento_persona = vd.id_persona INNER JOIN agora.informacion_proveedor ip ON ipn.num_documento_persona = ip.num_documento INNER JOIN core.ciudad c ON ipn.id_ciudad_expedicion_documento = c.id_ciudad INNER JOIN administrativa.escalafon_persona ep ON ip.id_proveedor = ep.id_persona_natural INNER JOIN administrativa.escalafon e ON e.id_escalafon = ep.id_escalafon INNER JOIN administrativa.dedicacion d ON vd.id_dedicacion = d.id_dedicacion WHERE vd.estado = TRUE AND vd.id_resolucion = " + idResolucion + ";").QueryRows(&temp)
 	if err == nil {
 		fmt.Println("Consulta exitosa")
 	}
@@ -37,27 +38,27 @@ func GetAllPrecontratado(idResolucion string) (arregloIDs []Precontratado) {
 }
 
 func GetOnePrecontratado(idResolucion string, idPersona string) (precontratado *Precontratado) {
-    o := orm.NewOrm()
-    var temp []*Precontratado
-    _, err := o.Raw("SELECT administrativa.vinculacion_docente.id id, agora.informacion_persona_natural.primer_nombre primer_nombre, agora.informacion_persona_natural.segundo_nombre segundo_nombre, agora.informacion_persona_natural.primer_apellido primer_apellido, agora.informacion_persona_natural.segundo_apellido segundo_apellido, agora.informacion_persona_natural.num_documento_persona documento, core.ciudad.nombre expedicion, administrativa.escalafon.nombre_escalafon categoria, administrativa.dedicacion.nombre_dedicacion dedicacion, administrativa.vinculacion_docente.numero_horas_semanales horas_semanales, administrativa.vinculacion_docente.numero_semanas semanas, administrativa.vinculacion_docente.id_proyecto_curricular proyecto_curricular FROM agora.informacion_persona_natural, core.ciudad, administrativa.escalafon_persona, administrativa.escalafon, administrativa.dedicacion, administrativa.vinculacion_docente WHERE agora.informacion_persona_natural.id_ciudad_expedicion_documento=core.ciudad.id_ciudad AND agora.informacion_persona_natural.num_documento_persona=administrativa.vinculacion_docente.id_persona AND administrativa.escalafon_persona.id_persona_natural=agora.informacion_persona_natural.num_documento_persona AND administrativa.vinculacion_docente.id_dedicacion=administrativa.dedicacion.id_dedicacion AND administrativa.escalafon_persona.id_escalafon=administrativa.escalafon.id_escalafon AND administrativa.vinculacion_docente.estado=TRUE AND administrativa.vinculacion_docente.id_resolucion = "+idResolucion+" AND agora.informacion_persona_natural.num_documento_persona="+idPersona+";").QueryRows(&temp)
-    if err == nil {
-        fmt.Println("Consulta exitosa")
-    }
+	o := orm.NewOrm()
+	var temp []*Precontratado
+	_, err := o.Raw("SELECT vd.id id, ipn.primer_nombre, ipn.segundo_nombre, ipn.primer_apellido, ipn.segundo_apellido, ipn.num_documento_persona documento, c.nombre expedicion, e.nombre_escalafon categoria, d.nombre_dedicacion dedicacion, vd.numero_horas_semanales horas_semanales, vd.numero_semanas semanas, vd.id_proyecto_curricular FROM administrativa.vinculacion_docente vd INNER JOIN agora.informacion_persona_natural ipn ON ipn.num_documento_persona = vd.id_persona INNER JOIN agora.informacion_proveedor ip ON ipn.num_documento_persona = ip.num_documento INNER JOIN core.ciudad c ON ipn.id_ciudad_expedicion_documento = c.id_ciudad INNER JOIN administrativa.escalafon_persona ep ON ip.id_proveedor = ep.id_persona_natural INNER JOIN administrativa.escalafon e ON e.id_escalafon = ep.id_escalafon INNER JOIN administrativa.dedicacion d ON vd.id_dedicacion = d.id_dedicacion WHERE vd.estado = TRUE AND vd.id_resolucion = " + idResolucion + " AND ipn.num_documento_persona = " + idPersona + ";").QueryRows(&temp)
+	if err == nil {
+		fmt.Println("Consulta exitosa")
+	}
 
-    if(temp!=nil && len(temp)>0){
-        return temp[0]
-    }else{
-        return nil
-    }
+	if temp != nil && len(temp) > 0 {
+		return temp[0]
+	} else {
+		return nil
+	}
 }
 
 func GetAllContratado(idResolucion string) (arregloIDs []Precontratado) {
-    o := orm.NewOrm()
-    var temp []Precontratado
-    _, err := o.Raw("SELECT administrativa.vinculacion_docente.id id, agora.informacion_persona_natural.primer_nombre primer_nombre, agora.informacion_persona_natural.segundo_nombre segundo_nombre, agora.informacion_persona_natural.primer_apellido primer_apellido, agora.informacion_persona_natural.segundo_apellido segundo_apellido, agora.informacion_persona_natural.num_documento_persona documento, core.ciudad.nombre expedicion, administrativa.escalafon.nombre_escalafon categoria, administrativa.dedicacion.nombre_dedicacion dedicacion, administrativa.vinculacion_docente.numero_horas_semanales horas_semanales, administrativa.vinculacion_docente.numero_semanas semanas, administrativa.vinculacion_docente.id_proyecto_curricular proyecto_curricular, argo.contrato_general.valor_contrato valor_contrato FROM agora.informacion_persona_natural, core.ciudad, administrativa.escalafon_persona, administrativa.escalafon, administrativa.dedicacion, administrativa.vinculacion_docente, argo.contrato_general WHERE agora.informacion_persona_natural.id_ciudad_expedicion_documento=core.ciudad.id_ciudad AND agora.informacion_persona_natural.num_documento_persona=administrativa.vinculacion_docente.id_persona AND administrativa.escalafon_persona.id_persona_natural=agora.informacion_persona_natural.num_documento_persona AND administrativa.vinculacion_docente.id_dedicacion=administrativa.dedicacion.id_dedicacion AND administrativa.escalafon_persona.id_escalafon=administrativa.escalafon.id_escalafon AND administrativa.vinculacion_docente.estado=TRUE AND administrativa.vinculacion_docente.numero_contrato=argo.contrato_general.numero_contrato AND administrativa.vinculacion_docente.vigencia=argo.contrato_general.vigencia AND administrativa.vinculacion_docente.id_resolucion = "+idResolucion+";").QueryRows(&temp)
-    if err == nil {
-        fmt.Println("Consulta exitosa")
-    }
+	o := orm.NewOrm()
+	var temp []Precontratado
+	_, err := o.Raw("SELECT vd.id id, ipn.primer_nombre, ipn.segundo_nombre, ipn.primer_apellido, ipn.segundo_apellido, ipn.num_documento_persona documento, c.nombre expedicion, e.nombre_escalafon categoria, d.nombre_dedicacion dedicacion, vd.numero_horas_semanales horas_semanales, vd.numero_semanas semanas, vd.id_proyecto_curricular, cg.valor_contrato FROM administrativa.vinculacion_docente vd INNER JOIN agora.informacion_persona_natural ipn ON ipn.num_documento_persona = vd.id_persona INNER JOIN agora.informacion_proveedor ip ON ipn.num_documento_persona = ip.num_documento INNER JOIN core.ciudad c ON ipn.id_ciudad_expedicion_documento = c.id_ciudad INNER JOIN administrativa.escalafon_persona ep ON ip.id_proveedor = ep.id_persona_natural INNER JOIN administrativa.escalafon e ON e.id_escalafon = ep.id_escalafon INNER JOIN administrativa.dedicacion d ON vd.id_dedicacion = d.id_dedicacion INNER JOIN argo.contrato_general cg ON (vd.numero_contrato = cg.numero_contrato AND vd.vigencia = cg.vigencia) WHERE vd.estado = TRUE AND vd.id_resolucion = " + idResolucion + ";").QueryRows(&temp)
+	if err == nil {
+		fmt.Println("Consulta exitosa")
+	}
 
-    return temp
+	return temp
 }
