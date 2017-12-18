28416615288f6361d818a8ae043c1a4242c9ede3
Kmilo9433 <camilo@ulises.udistritaloas.edu.co>
2017-12-18 13:46:24 -0500

    Se agrega funcion personalizada para resolucion_vinculacion expedida o aprobada

--- controllers/resolucion_vinculacion.go
+++ controllers/resolucion_vinculacion.go
@@ -11,6 +11,7 @@ type ResolucionVinculacionController struct {
 
 func (c *ResolucionVinculacionController) URLMapping() {
 	c.Mapping("GetAll", c.GetAll)
+	c.Mapping("GetAllAprobada", c.GetAllAprobada)
 
 }
 
@@ -28,6 +29,25 @@ func (c *ResolucionVinculacionController) URLMapping() {
 // @router / [get]
 func (c *ResolucionVinculacionController) GetAll() {
 	listaResoluciones := models.GetAllResolucionVinculacion()
+	c.Ctx.Output.SetStatus(201)
+	c.Data["json"] = listaResoluciones
+	c.ServeJSON()
+}
+
+// GetAllAprobada ...
+// @Title Get All
+// @Description get ResolucionVinculacion
+// @Param	query	query	string	false	"Filter. e.g. col1:v1,col2:v2 ..."
+// @Param	fields	query	string	false	"Fields returned. e.g. col1,col2 ..."
+// @Param	sortby	query	string	false	"Sorted-by fields. e.g. col1,col2 ..."
+// @Param	order	query	string	false	"Order corresponding to each sortby field, if single value, apply to all sortby fields. e.g. desc,asc ..."
+// @Param	limit	query	string	false	"Limit the size of result set. Must be an integer"
+// @Param	offset	query	string	false	"Start position of result set. Must be an integer"
+// @Success 200 {object} models.ResolucionVinculacionDocente
+// @Failure 403
+// @router /Aprobada [get]
+func (c *ResolucionVinculacionController) GetAllAprobada() {
+	listaResoluciones := models.GetAllResolucionAprobada()
 	c.Ctx.Output.SetStatus(201)
 	c.Data["json"] = listaResoluciones
 	c.ServeJSON()
