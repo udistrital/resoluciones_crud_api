8b0fd8c330ffc87c1eb30ef2e30a16279bac8252
Oscar Duarte <o.duarte7@gmail.com>
2018-01-03 16:00:06 -0500

    Se agrega relatedSel en el modelo item_informe_tipo_contrato

--- models/item_informe_tipo_contrato.go
+++ models/item_informe_tipo_contrato.go
@@ -47,7 +47,7 @@ func GetItemInformeTipoContratoById(id int) (v *ItemInformeTipoContrato, err err
 func GetAllItemInformeTipoContrato(query map[string]string, fields []string, sortby []string, order []string,
 	offset int64, limit int64) (ml []interface{}, err error) {
 	o := orm.NewOrm()
-	qs := o.QueryTable(new(ItemInformeTipoContrato))
+	qs := o.QueryTable(new(ItemInformeTipoContrato)).RelatedSel()
 	// query k=v
 	for k, v := range query {
 		// rewrite dot-notation to Object__Attribute
