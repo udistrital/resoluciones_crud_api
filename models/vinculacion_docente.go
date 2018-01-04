bd33c066b53025596d4fc4ef48dbece7622113b0
Kmilo9433 <camilo@ulises.udistritaloas.edu.co>
2018-01-04 14:57:27 -0500

    Se da una solucion al problema de la uk en vinculacion_docente

--- models/vinculacion_docente.go
+++ models/vinculacion_docente.go
@@ -12,8 +12,8 @@ import (
 
 type VinculacionDocente struct {
 	Id                   int                           `orm:"column(id);pk;auto"`
-	NumeroContrato       string                        `orm:"column(numero_contrato);null"`
-	Vigencia             int                           `orm:"column(vigencia);null"`
+	NumeroContrato       *string                       `orm:"column(numero_contrato);null"`
+	Vigencia             *int                          `orm:"column(vigencia);null"`
 	IdPersona            string                        `orm:"column(id_persona)"`
 	NumeroHorasSemanales int                           `orm:"column(numero_horas_semanales)"`
 	NumeroSemanas        int                           `orm:"column(numero_semanas)"`
