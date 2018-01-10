5c3560b4d2211d9205e9e02d20967be0f66c5819
Oscar Duarte <o.duarte7@gmail.com>
2018-01-10 11:42:21 -0500

    Se agrega RelatedSel en el modelo de pago_mensual

--- models/pago_mensual.go
+++ models/pago_mensual.go
@@ -55,7 +55,7 @@ func GetPagoMensualById(id int) (v *PagoMensual, err error) {
 func GetAllPagoMensual(query map[string]string, fields []string, sortby []string, order []string,
 	offset int64, limit int64) (ml []interface{}, err error) {
 	o := orm.NewOrm()
-	qs := o.QueryTable(new(PagoMensual))
+	qs := o.QueryTable(new(PagoMensual)).RelatedSel()
 	// query k=v
 	for k, v := range query {
 		// rewrite dot-notation to Object__Attribute
