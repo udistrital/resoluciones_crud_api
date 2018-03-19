8cb78b279223cc69dcb9f54416908aca9a393622
Oscar Duarte <o.duarte7@gmail.com>
2018-03-18 22:29:14 -0500

    Se agregan servicios de aprobación masiva para el módulo de contratistas

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -4695,6 +4695,14 @@ func init() {
 			MethodParams: param.Make(),
 			Params: nil})
 
+	beego.GlobalControllerRouter["github.com/udistrital/administrativa_crud_api/controllers:TrAprobacionMasivaSoportesContratistasController"] = append(beego.GlobalControllerRouter["github.com/udistrital/administrativa_crud_api/controllers:TrAprobacionMasivaSoportesContratistasController"],
+		beego.ControllerComments{
+			Method: "AprobarSoportesContratistas",
+			Router: `/`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
 	beego.GlobalControllerRouter["github.com/udistrital/administrativa_crud_api/controllers:TrNecesidadController"] = append(beego.GlobalControllerRouter["github.com/udistrital/administrativa_crud_api/controllers:TrNecesidadController"],
 		beego.ControllerComments{
 			Method: "Post",
